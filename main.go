1695e838eb37734c69c00a4011199cf40ace453c
bu4ak <igorbu4@gmail.com>
2018-12-09 23:42:04 +0700

    Closes #1

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"strings"
+)
 
 func main() {
 	r := gin.Default()
-	r.GET("/api/send", func(c *gin.Context) {
+	r.Any("/api/send", func(c *gin.Context) {
+
 		c.JSON(200, gin.H{
-			"message": "ok",
+			"token": getToken(c),
 		})
 	})
 	r.Run()
+}
+
+type Req struct {
+	Token string `json:"token"`
+}
+
+func getToken(c *gin.Context) string {
+	key := "token"
+	var req Req
+	token := c.DefaultQuery(key, c.DefaultPostForm(key, c.GetHeader(key)))
+	if token == "" {
+		if err := c.BindJSON(&req); err == nil {
+			token = req.Token
+		}
+	}
+	return strings.TrimSpace(token)
 }
