2696f8278fffae1be5d1e5e59ebca98e216d8673
bu4ak <igorbu4@gmail.com>
2018-12-12 23:57:10 +0700

    Resolve #1

--- main.go
+++ main.go
@@ -2,32 +2,33 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"strings"
 )
 
 func main() {
-	r := gin.Default()
-	r.Any("/api/send", func(c *gin.Context) {
+	r := gin.New()
+	if gin.IsDebugging() {
+		r.Use(gin.Logger())
+	}
+	r.Use(gin.Recovery())
 
-		c.JSON(200, gin.H{
-			"token": getToken(c),
-		})
+	r.Any("/api/send", func(c *gin.Context) {
+		var req struct {
+			Token   string `json:"token"`
+			Message string `json:"message"`
+		}
+		req.Token = get(c, "token")
+		req.Message = get(c, "message")
+
+		if c.ContentType() == gin.MIMEJSON {
+			c.BindJSON(&req)
+		}
+		c.JSON(http.StatusOK, req)
 	})
 	r.Run()
 }
 
-type Req struct {
-	Token string `json:"token"`
-}
-
-func getToken(c *gin.Context) string {
-	key := "token"
-	var req Req
-	token := c.DefaultQuery(key, c.DefaultPostForm(key, c.GetHeader(key)))
-	if token == "" {
-		if err := c.BindJSON(&req); err == nil {
-			token = req.Token
-		}
-	}
-	return strings.TrimSpace(token)
+func get(c *gin.Context, key string) string {
+	return strings.TrimSpace(c.DefaultQuery(key, c.DefaultPostForm(key, c.GetHeader(key))))
 }
