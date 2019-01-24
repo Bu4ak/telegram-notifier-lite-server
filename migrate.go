f7f5bb45faa79bde29db8e9aeacb2ac15e16e962
bu4ak <igorbu4@gmail.com>
2019-01-24 21:24:26 +0700

    Closes #5

---
+++ migrate.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/DavidHuie/gomigrate"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	migrator, _ := gomigrate.NewMigrator(db, gomigrate.Postgres{}, "./migrations")
+	err = migrator.Migrate()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
